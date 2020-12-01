852e95b4f5cabae4f3368dce4232857b488af8aa
Saad <f178024@nu.eu.pk>
2020-12-01 18:24:52 +0500

    changed menu

--- components/Menu.js
+++ components/Menu.js
@@ -1,22 +1,23 @@
 import { useState } from 'react'
 import MenuItem from './MenuItem'
-import { faChartPie, faChalkboardTeacher, faBook, faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
+import { faChartPie, faChalkboardTeacher, faBook, faCalendarAlt, faUserGraduate } from '@fortawesome/free-solid-svg-icons'
 
 export default function Menu() {
     const [items, setItems] = useState([
         { name: 'dashboard', link: '/', icon: faChartPie },
         { name: 'class', link: '/class', icon: faChalkboardTeacher },
-        { name: 'students', link: '/students', icon: faChalkboardTeacher },
+        { name: 'students', link: '/students', icon: faUserGraduate },
         { name: 'courses', link: '/courses', icon: faBook },
-        { name: 'schedule', link: '/schedule', icon: faCalendarAlt },
         { name: 'quiz', link: '/quiz', icon: faCalendarAlt }
     ])
 
     return (
-        <nav>
-            <ul className="p-0 m-0 w-32 flex flex-col pt-32 h-screen">
-                {items.map(item => <MenuItem name={item.name} link={item.link} icon={item.icon} />)}
-            </ul>
-        </nav>
+        <div>
+            <nav>
+                <ul className="p-0 m-0 w-32 flex flex-col pt-32 h-screen">
+                    {items.map(item => <MenuItem name={item.name} link={item.link} icon={item.icon} />)}
+                </ul>
+            </nav>
+        </div>
     )
 }
