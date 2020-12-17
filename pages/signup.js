0a6982efe96eff816a6fc4bdab917b8a9d542d95
Saad <f178024@nu.eu.pk>
2020-12-17 16:57:20 +0500

    changed signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import Link from "next/link";
 import axios from 'axios'
 import {toast} from 'react-toastify'
 
@@ -9,7 +10,9 @@ export default function signup() {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
 
-  function handleSingup() {
+  function handleSignup(e) {
+    e.preventDefault()
+
     axios.post('/api/signup', {
       cnic: CNIC,
       email,
@@ -26,37 +29,30 @@ export default function signup() {
   return (
     <div className="flex flex-col items-center pt-24">
       <h1>Create a new account</h1>
-      <div className="flex flex-col p-8 shadow sm:w-full">
+      <form onSubmit={handleSignup} className="flex flex-col p-8 px-12 shadow sm:w-full">
         <input type="text" name="cnic" id="" placeholder="CNIC" onChange={e => setCNIC(e.target.value)} />
         <input type="email" name="email" id="" placeholder="Email" onChange={e => setEmail(e.target.value)} />
         <input type="text" name="phone" id="" placeholder="Phone Number" onChange={e => setPhone(e.target.value)} />
         <input type="username" name="password" id="" placeholder="Username" onChange={e => setUsername(e.target.value)} />
         <input type="password" name="password" id="" placeholder="Password" onChange={e => setPassword(e.target.value)} />
         <input type="password" name="password" id="" placeholder="Re-enter Password" />
-        <label htmlFor="">Date of Birth</label>
-        <div className="flex flex-row">
-          <select>
-            <option value="" selected disabled>Day</option>
-          </select>
-          <select>
-            <option value="" selected disabled>Month</option>
-          </select>
-          <select>
-            <option value="" selected disabled>Year</option>
-          </select>
-        </div>
+        <label htmlFor="dob">Date of Birth</label>
+        <input type="date" name="dob" id="dob" />
         <select>
           <option value="" selected disabled>Account Type</option>
           <option value="">Student</option>
           <option value="">Teacher</option>
           <option value="">Parent</option>
         </select>
-        <div className="flex justify-between">
+        <div className="flex justify-between pt-4">
           <a href="" className="pr-8">Forgot Password?</a>
-          <input type="button" value="Sign Up" onClick={handleSingup} />
+          <input type="submit" value="Sign Up"  />
         </div>
-      </div>
-      <a href="/signin" className="pt-4">Already have an account? Sing in</a>
+      </form>
+      <Link href="/signin">
+        <a className="pt-4">Already have an account? Sing in</a>
+      </Link>
+
     </div>
   )
 }
